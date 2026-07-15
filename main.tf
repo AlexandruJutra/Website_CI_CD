293a0332020688fac8d346dba14cb4f3c9bbbafb
jua2clj <alexandru.jutra@ro.bosch.com>
2026-07-15 14:35:32 +0300

    commit

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
 terraform {
   # Configure the backend to use an S3 bucket for storing the Terraform state file
   backend "s3" {
-    bucket = "terraform-state-bucket-md-2026"
-    key    = "dev/terraform.tfstate"
-    region = "eu-north-1"
-    encrypt = true
+    bucket       = "terraform-state-bucket-md-2026"
+    key          = "dev/terraform.tfstate"
+    region       = "eu-north-1"
+    encrypt      = true
     use_lockfile = true
   }
 
-  #set the required providers for the project
+  # set the required providers for the project
   required_providers {
     aws = {
       source  = "hashicorp/aws"
